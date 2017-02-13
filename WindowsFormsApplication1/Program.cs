49ad9a972f2209d31aa7eaba54c0d6763c5a3be4
shiqin1982 <shiqin5898@163.com>
2017-02-13 11:00:48 +0800

    WindowsAPP
    Signed-off-by: shiqin1982 <shiqin5898@163.com>

---
+++ WindowsFormsApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
